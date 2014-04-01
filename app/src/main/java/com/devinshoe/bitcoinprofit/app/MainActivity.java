1e2ed849b1e1580946a7290d5f5cdf38dda1c510
Devin Shoemaker <devinshoe@gmail.com>
2014-03-31 19:13:19 -0500

    Calculate daily BTC and daily USD

--- app/src/main/java/com/devinshoe/bitcoinprofit/app/MainActivity.java
+++ app/src/main/java/com/devinshoe/bitcoinprofit/app/MainActivity.java
@@ -18,13 +18,17 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void ocCalculate(View v) {
+        double dailyBtc;
         EditText etGhs = (EditText) findViewById(R.id.etGhs);
+        EditText etUsd = (EditText) findViewById(R.id.etUsd);
         TextView tvDgmOut = (TextView) findViewById(R.id.tvDgmOut);
+        TextView tvDailyBtcOut = (TextView) findViewById(R.id.tvDailyBtcOut);
+        TextView tvDailyUsdOut = (TextView) findViewById(R.id.tvDailyUsdOut);
 
         // check whether the user has entered the Gh/s
         if (!"".equals(etGhs.getText().toString())) {
             // declare and initialize variables for Gh/s and DGM
-            double ghs, dgm, dailyBtc;
+            double ghs, dgm;
 
             ghs = Double.parseDouble(etGhs.getText().toString());
             dgm = ghs * 0.000064179; // average DGM
@@ -32,8 +36,23 @@ public class MainActivity extends ActionBarActivity {
 
             // set DGM to TextView
             tvDgmOut.setText(Double.toString((double)Math.round(dgm * 1000000000) / 1000000000));
-        }
 
+            // calculate daily BTC
+            tvDailyBtcOut.setText(Double.toString(dailyBtc));
+
+            // check whether the user has entered the exchange rate
+            if (!"".equals(etUsd.getText().toString())) {
+                // declare and initialize variables for Gh/s and DGM
+                double exchangeRate, dailyUsd;
+
+                // convert daily BTC to USD
+                exchangeRate = Double.parseDouble(etUsd.getText().toString());
+                dailyUsd = dailyBtc * exchangeRate;
+
+                // set USD to TextView
+                tvDailyUsdOut.setText(Double.toString((double)Math.round(dailyUsd * 100) / 100));
+            }
+        }
     }
 
     @Override
